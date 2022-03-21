28bb09a76b91400a0ca8aad0259731adc962f9c7
jym0415 <jym9504@gmail.com>
2022-03-21 23:33:25 +0900

    강원천문대

---
+++ js/slider.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+var count = 0;
+var classNm = "show"
+var total = $("#slider li").length; 
+
+
+setInterval(function(){
+count ++
+count %= total
+
+  $("#slider li").eq(count).addClass(classNm).siblings().removeClass(classNm)
+},3000)
+
+
+
+
+$('.tab h3').click(function(){
+    $('.tab > div').removeClass('on');
+    $(this).parent().addClass('on');
+})
+
+
+
+})
